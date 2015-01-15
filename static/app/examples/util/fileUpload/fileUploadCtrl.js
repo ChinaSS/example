2f1c9e67d11b61606f47e77d251cd74c5e872b66
YiYing <yiyingqq@gmail.com>
2015-01-15 17:16:36 +0800

    fileUpload更新

---
+++ static/app/examples/util/fileUpload/fileUploadCtrl.js
@@ -0,0 +1,27 @@
+define(["jquery","UtilDir/util","CodeMirror","css!CodeMirrorCSS"],function($,Util){
+
+    return function($compile,$scope){
+        var path = "app/examples/util";
+
+        //初始化代码编辑面板
+        var editor = CodeMirror.fromTextArea(document.getElementById("exampleSource"), {
+            lineWrapping:true, //是否显示scroll
+            lineNumbers: true, //是否显示number
+            styleActiveLine: true,
+            matchBrackets: true,
+            mode:"htmlmixed",
+            viewportMargin: Infinity
+        });
+        $scope.runJS = function(){
+            var link = $compile(editor.getValue());
+            $("#exampleInstance").empty().html(link($scope));
+        };
+
+        $scope.showAPI = function(){
+            Util.slidebar({
+                url:path+"/fileUpload/views/doc.html",
+                width:"800px"
+            });
+        };
+    };
+});
--- static/app/routerMap.js
+++ static/app/routerMap.js
@@ -12,11 +12,15 @@ define(function() {
                 routerPath : '/dialog',                                     //访问路径
                 templateUrl : 'app/examples/util/dialog/views/dialog.html'  //html文件
             },
+            /*
+             * 附件上传样例
+             * ep_DialogCtrl
+             */
             {
-                ctrlName : "ep_DemoViewCtrl",                                 //ctrl名称
-                ctrlUrl : "demoViewDir/demoViewCtrl",             //ctrl路径
-                routerPath : '/demoView/:id',                                     //访问路径
-                templateUrl : 'app/demoView/demoView.html'  //html文件
+                ctrlName : "ep_fileUploadCtrl",
+                ctrlUrl : "examplesDir/util/fileUpload/fileUploadCtrl",
+                routerPath : '/fileUpload',
+                templateUrl : 'app/examples/util/fileUpload/views/fileUpload.html'
             }
         ]
     };
